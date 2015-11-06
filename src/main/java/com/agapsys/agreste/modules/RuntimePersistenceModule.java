20f1fa1accdb7e96c662fcd467b29494f3f633c0
Leandro Oliveira <leandro@agapsys.com>
2015-11-06 20:51:39 -0300

    Added custom persistence modules

---
+++ src/main/java/com/agapsys/agreste/modules/RuntimePersistenceModule.java
@@ -0,0 +1,20 @@
+/* 
+ * Copyright (C) 2015 Agapsys Tecnologia - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ */
+
+package com.agapsys.agreste.modules;
+
+import javax.persistence.EntityManager;
+import javax.persistence.FlushModeType;
+
+public class RuntimePersistenceModule extends com.agapsys.web.toolkit.RuntimePersistenceModule{
+
+	@Override
+	protected EntityManager _getEntityManager() {
+		EntityManager em = super._getEntityManager();
+		em.setFlushMode(FlushModeType.COMMIT);
+		return em;
+	}
+}
