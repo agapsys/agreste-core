47da491a21b6b6aaa3c028fab0bc011e01c759f9
Leandro Oliveira <leandro@agapsys.com>
2016-01-09 15:52:46 -0300

    Test utilities refactoring

--- src/main/java/com/agapsys/agreste/test/TestUtils.java
+++ src/main/java/com/agapsys/agreste/test/TestUtils.java
@@ -5,10 +5,18 @@
  */
 package com.agapsys.agreste.test;
 
+import com.agapsys.agreste.servlets.BaseServlet;
+import com.agapsys.http.HttpDelete;
+import com.agapsys.http.HttpGet;
+import com.agapsys.http.HttpHead;
+import com.agapsys.http.HttpOptions;
+import com.agapsys.http.HttpRequest;
 import com.agapsys.http.HttpResponse.StringResponse;
+import com.agapsys.http.HttpTrace;
 import com.agapsys.http.StringEntityRequest;
 import com.agapsys.utils.console.printer.ConsoleColor;
 import com.agapsys.utils.console.printer.ConsolePrinter;
+import com.agapsys.web.action.dispatcher.HttpMethod;
 import com.agapsys.web.toolkit.AbstractWebApplication;
 import com.agapsys.web.toolkit.JsonSerializer;
 import com.agapsys.web.toolkit.Module;
@@ -38,6 +46,159 @@ public class TestUtils {
 			singleton = new TestUtils();
 		
 		return singleton;
+	}
+	
+	public static class RestEndpoint {
+		public final HttpMethod method;
+		public final String uri;
+		public final String[] uriParamNames;
+		
+		private final TestUtils testUtils = getInstance();
+		
+		private boolean isEntityMethod(HttpMethod method) {
+			switch(method) {
+				case POST:
+				case PUT:
+				case PATCH:
+					return true;
+			}
+			
+			return false;
+		}
+		
+		public RestEndpoint(HttpMethod method, String uri, String...uriParamNames) {			
+			if (method == null)
+				throw new IllegalArgumentException("Null HTTP method");
+			
+			if (isEntityMethod(method))
+				throw new UnsupportedOperationException("Unsupported method: " + method.name());
+			
+			if (uri == null || uri.trim().isEmpty())
+				throw new IllegalArgumentException("Null/Empty URI");
+			
+			this.method = method;
+			this.uri = uri;
+			this.uriParamNames = uriParamNames;
+		}
+		
+		public HttpRequest getRequest(Object...uriParams) {
+			StringBuilder uriFormat = new StringBuilder(uri);
+			
+			boolean firstParam = true;
+			for (String paramName : uriParamNames) {
+				if (firstParam) {
+					uriFormat.append("?");
+					firstParam = false;
+				} else {
+					uriFormat.append("&");
+				}
+
+				uriFormat.append(String.format("%s=%%s", paramName));
+			}
+
+			switch (method) {
+				case DELETE:
+					return new HttpDelete(uriFormat.toString(), uriParams);
+
+				case GET:
+					return new HttpGet(uriFormat.toString(), uriParams);
+
+				case HEAD:
+					return new HttpHead(uriFormat.toString(), uriParams);
+
+				case OPTIONS:
+					return new HttpOptions(uriFormat.toString(), uriParams);
+
+				case TRACE:
+					return new HttpTrace(uriFormat.toString(), uriParams);
+
+				default:
+					throw new UnsupportedOperationException("Unsupported method: " + method.name());
+			}
+		}
+		
+		@Override
+		public String toString() {
+			return String.format("%s %s", method.name(), uri);
+		}
+	}
+	
+	public static class EntityRestEndpoint {
+		public final HttpMethod method;
+		public final String uri;
+		public final String[] uriParamNames;
+		private JsonSerializer serializer;
+		
+		private final TestUtils testUtils = getInstance();
+		
+		private boolean isEntityMethod(HttpMethod method) {
+			switch(method) {
+				case POST:
+				case PUT:
+				case PATCH:
+					return true;
+			}
+			
+			return false;
+		}
+
+		public EntityRestEndpoint(HttpMethod method, JsonSerializer serializer, String uri, String...uriParamNames) {			
+			if (method == null)
+				throw new IllegalArgumentException("Null HTTP method");
+			
+			if (!isEntityMethod(method))
+				throw new UnsupportedOperationException("Unsupported method: " + method.name());
+			
+			if (serializer == null)
+				throw new IllegalArgumentException("Serializer cannot be null");
+			
+			if (uri == null || uri.trim().isEmpty())
+				throw new IllegalArgumentException("Null/Empty URI");
+			
+			this.method = method;
+			this.uri = uri;
+			this.uriParamNames = uriParamNames;
+			this.serializer = serializer;
+		}
+		
+		public EntityRestEndpoint(HttpMethod method, String uri, String...uriParamNames) {
+			this(method, (JsonSerializer) BaseServlet.DEFAULT_SERIALIZER, uri, uriParamNames);
+		}
+		
+		public HttpRequest getRequest(Object dto, Object...uriParams) {
+			StringBuilder uriFormat = new StringBuilder(uri);
+			
+			boolean firstParam = true;
+			for (String paramName : uriParamNames) {
+				if (firstParam) {
+					uriFormat.append("?");
+					firstParam = false;
+				} else {
+					uriFormat.append("&");
+				}
+
+				uriFormat.append(String.format("%s=%%s", paramName));
+			}
+				
+			Class<? extends StringEntityRequest> requestClass;
+			switch (method) {
+				case POST:
+					requestClass = StringEntityRequest.StringEntityPost.class;
+					break;
+				case PUT:
+					requestClass = StringEntityRequest.StringEntityPut.class;
+					break;
+
+				case PATCH:
+					requestClass = StringEntityRequest.StringEntityPatch.class;
+					break;
+
+				default:
+					throw new UnsupportedOperationException("Unsupported method: " + method.name());
+			}
+
+			return testUtils.createJsonRequest(requestClass, serializer, uriParams[0], uri);
+		}
 	}
 	// =========================================================================
 	
