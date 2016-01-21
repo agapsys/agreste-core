539f4d8ab4d0dcb4e32b0c7f4b524fedfcc15c9b
Leandro Oliveira <leandro@agapsys.com>
2016-01-20 23:56:33 -0300

    Added client exceptions

---
+++ src/main/java/com/agapsys/agreste/exceptions/InvalidDataException.java
@@ -0,0 +1,14 @@
+/* 
+ * Copyright (C) 2016 Agapsys Tecnologia - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ */
+
+package com.agapsys.agreste.exceptions;
+
+public class InvalidDataException extends BadRequestException {
+
+	public InvalidDataException(String message, Object... args) {
+		super(message, args);
+	}
+}
---
+++ src/main/java/com/agapsys/agreste/exceptions/RateLimitingException.java
@@ -0,0 +1,21 @@
+/* 
+ * Copyright (C) 2016 Agapsys Tecnologia - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ */
+
+package com.agapsys.agreste.exceptions;
+
+public class RateLimitingException extends ClientException {
+	
+	// CLASS SCOPE =============================================================
+	public static final int CODE = 429;
+	// =========================================================================
+
+	// INSTANCE SCOPE ==========================================================
+	public RateLimitingException(String message, Object...args) {
+		super(CODE, message, args);
+	}
+	// =========================================================================
+	
+}
