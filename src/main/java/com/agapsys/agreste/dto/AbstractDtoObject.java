17b4cab0429d21f8b50a99b64e208e78590e021c
Leandro Oliveira <leandro@agapsys.com>
2016-02-16 21:09:47 -0300

    Required may apply to AbstractDtoObject attributes

--- src/main/java/com/agapsys/agreste/dto/AbstractDtoObject.java
+++ src/main/java/com/agapsys/agreste/dto/AbstractDtoObject.java
@@ -18,43 +18,9 @@ package com.agapsys.agreste.dto;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
-import java.util.Collection;
-import java.util.LinkedHashSet;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Set;
 
 public abstract class AbstractDtoObject {
-	// CLASS SCOPE =============================================================
-	public static <T extends AbstractDtoObject> Collection<T> getDtoCollection(Class<T> dtoClass, Collection collection) {
-		try {
-			Collection<T> destCollection;
-			
-			if (collection instanceof Set) {
-				destCollection = new LinkedHashSet<>();
-			} else if (collection instanceof List) {
-				destCollection = new LinkedList<>();
-			} else {
-				throw new UnsupportedOperationException("Unsupported collection: " + collection.getClass().getName());
-			} 
-						
-			for (Object object : collection) {
-				destCollection.add(dtoClass.getConstructor(object.getClass()).newInstance(object));
-			}
-			
-			return destCollection;
-			
-		} catch (Throwable t) {
-			if (t instanceof RuntimeException) {
-				throw (RuntimeException) t;
-			} else {
-				throw new RuntimeException(t);
-			}
-		}
-	}
-	// =========================================================================
-	
-	// INSTANCE SCOPE ==========================================================
+
 	public void validate() throws DtoValidationException {
 		Field[] fields = getClass().getDeclaredFields();
 		
@@ -66,19 +32,6 @@ public abstract class AbstractDtoObject {
 			
 			try {
 				
-				if (AbstractDtoObject.class.isAssignableFrom(field.getType())) {
-					AbstractDtoObject dtoField = (AbstractDtoObject) field.get(this);
-					if (dtoField != null) {
-						try {
-							dtoField.validate();
-						} catch (DtoValidationException ex) {
-							String fullPathFieldName = String.format("%s.%s", field.getName(), ex.getFieldName());
-							
-							throw new DtoValidationException(fullPathFieldName, ex.getUnformattedMessage());
-						}
-					}
-				}
-
 				DtoRequired[] annotations = field.getAnnotationsByType(DtoRequired.class);
 				DtoRequired annotation;
 
@@ -99,11 +52,25 @@ public abstract class AbstractDtoObject {
 					}
 
 				}
+				
+				if (AbstractDtoObject.class.isAssignableFrom(field.getType())) {
+					AbstractDtoObject dtoField = (AbstractDtoObject) field.get(this);
+					if (dtoField != null) {
+						try {
+							dtoField.validate();
+						} catch (DtoValidationException ex) {
+							String fullPathFieldName = String.format("%s.%s", field.getName(), ex.getFieldName());
+
+							throw new DtoValidationException(fullPathFieldName, ex.getUnformattedMessage());
+						}
+					}
+				}
 
 			} catch (IllegalAccessException | IllegalArgumentException ex) {
 				throw new RuntimeException(ex);
 			}
+			
 		}
 	}
-	// =========================================================================
+	
 }
