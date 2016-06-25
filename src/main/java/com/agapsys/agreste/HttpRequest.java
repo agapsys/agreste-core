0097e585dc47c01d70e33b2e5fa158c3035502cb
Leandro Oliveira <leandro@agapsys.com>
2016-06-25 14:04:10 -0300

    Fixed error message management

--- src/main/java/com/agapsys/agreste/HttpRequest.java
+++ src/main/java/com/agapsys/agreste/HttpRequest.java
@@ -16,6 +16,7 @@
 package com.agapsys.agreste;
 
 import com.agapsys.rcf.exceptions.BadRequestException;
+import java.util.regex.Pattern;
 import javax.servlet.http.HttpServletRequest;
 
 /**
@@ -23,7 +24,15 @@ import javax.servlet.http.HttpServletRequest;
  * @author Leandro Oliveira (leandro@agapsys.com)
  */
 public class HttpRequest extends com.agapsys.rcf.HttpRequest {
+	// STATIC SCOPE ============================================================
+	private static final String PARAM_VALUE_TOKEN = "${param}";
 
+	private static String formatErrorMessage(String errorMessage, String paramValue) {
+		return errorMessage.replaceAll(Pattern.quote(PARAM_VALUE_TOKEN), paramValue);
+	}
+	// =========================================================================
+
+	// INSTANCE SCOPE ==========================================================
 	public HttpRequest(HttpExchange exchange, HttpServletRequest coreRequest) {
 		super(exchange, coreRequest);
 	}
@@ -62,30 +71,60 @@ public class HttpRequest extends com.agapsys.rcf.HttpRequest {
 		return readParameterObject(getExchange().getParamMapSerializer(), dtoClass);
 	}
 
-	public <T> T getOptionalParameter(Class<T> targetClass, String paramName, T defaultValue) throws BadRequestException {
+	/**
+	 * Returns an optional request parameter.
+	 *
+	 * @param <T> expected parameter type.
+	 * @param targetClass expected parameter type,
+	 * @param paramName parameter name.
+	 * @param defaultValue parameter default value.
+	 * @param errorMessage error message if conversion to expected type fails. An especial token ${param} will be replaced by parameter value.
+	 * @param errMsgArgs optional error message format args.
+	 * @return parameter value
+	 * @throws BadRequestException if it was not possible to retrieve parameter value.
+	 */
+	public <T> T getOptionalParameter(Class<T> targetClass, String paramName, T defaultValue, String errorMessage, Object...errMsgArgs) throws BadRequestException {
 		try {
 			String paramValue = getOptionalParameter(paramName, null);
 
 			if (paramValue == null) return defaultValue;
 
+			errorMessage = formatErrorMessage(errorMessage, paramValue);
+
 			return getExchange().getParamMapSerializer().getParameter(paramValue, targetClass);
 
 		} catch (ParamMapSerializer.SerializerException ex) {
-			throw new BadRequestException(ex.getMessage());
+			throw new BadRequestException(errorMessage, errMsgArgs);
 		}
 	}
 
+	public final <T> T getOptionalParameter(Class<T> targetClass, String paramName, T defaultValue) throws BadRequestException {
+		return getOptionalParameter(targetClass, paramName, defaultValue, "Invalid value for parameter '%s': ${param}", paramName);
+	}
+
+	/**
+	 * Returns a mandatory request parameter.
+	 *
+	 * @param <T> expected parameter type.
+	 * @param targetClass expected parameter type.
+	 * @param paramName parameter name.
+	 * @param errorMessage error message if conversion to expected type fails. An especial token ${param} will be replaced by parameter value.
+	 * @param errMsgArgs optional error message format args.
+	 * @return parameter value
+	 * @throws BadRequestException if it was not possible to retrieve parameter value.
+	 */
 	public <T> T getMandatoryParameter(Class<T> targetClass, String paramName, String errorMessage, Object...errMsgArgs) throws BadRequestException {
 		try {
-			String paramValue = getMandatoryParameter(paramName, errorMessage, errMsgArgs);
+			String paramValue = getMandatoryParameter(paramName, "Missing parameter: %s", paramName);
+			errorMessage = formatErrorMessage(errorMessage, paramValue);
 			return getExchange().getParamMapSerializer().getParameter(paramValue, targetClass);
 		} catch (ParamMapSerializer.SerializerException ex) {
-			throw new BadRequestException(ex.getMessage());
+			throw new BadRequestException(errorMessage, errMsgArgs);
 		}
 	}
 
 	public final <T> T getMandatoryParameter(Class<T> targetClass, String paramName) throws BadRequestException {
-		return getMandatoryParameter(targetClass, paramName, "Missing parameter: %s", paramName);
+		return getMandatoryParameter(targetClass, paramName, "Invalid value for parameter '%s': ${param}", paramName);
 	}
-
+	// =========================================================================
 }
